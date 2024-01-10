83a3fc91d263a37e0fbd8c1078205f713f19cfae
Abhishek patel <111166632+Abhishek5137@users.noreply.github.com>
2024-01-11 01:18:25 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,16 @@
+const cont = document.querySelector('.cont')
+        const cont1 = document.querySelector('.cont1')
+        const button =document.querySelector('button')
+        button.addEventListener('click',()=>{
+            const one =  `${Math.floor(Math.random()*255)}`;
+            const two =  `${Math.floor(Math.random()*255)}`;
+            const three =  `${Math.floor(Math.random()*255)}`;
+            const back=`rgb(${one},${two},${three})`;
+            const back1=`rgb(${two},${three},${one})`
+            console.log(back)
+            console.log(back1)
+            let css = `background-image: linear-gradient(to right, ${back}, ${back1});`
+             cont.style = css;
+             //cont1.style.backgroundColor=css1;
+            
+        })
